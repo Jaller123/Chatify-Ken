18c1e07b5a15b86e5a1300cd51a9c42f3ae7dd83
Kenatho <89846381+Kenatho@users.noreply.github.com>
2024-08-20 16:01:01 +0200

    Authorized JWT into chat component however haven't fetched messages yet

--- chatify/src/components/Chat.jsx
+++ chatify/src/components/Chat.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 const Chat = () => {
+
+  const [messages, setMessages] = useState([])
+  const token = localStorage.getItem('userToken')
+
+  useEffect(() => {
+    if (!token)
+    {
+      console.log("The JWT token is not found")
+    }
+    
+
+ 
+
+  fetch('https://chatify-api.up.railway.app/messages',
+    {
+      method: 'GET',
+      headers:  
+      {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }, 
+    })
+
+    .then(res => res.json())
+    .then(data =>
+    {
+     setMessages(data) 
+     console.log(token)
+    })
+  }, [token])
+
   return (
-    <div></div>
+    <div>
+      <h2>Chat Messages</h2>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>
+            
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
